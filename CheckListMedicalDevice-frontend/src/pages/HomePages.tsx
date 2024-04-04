3e0bcd5b36a8ac0b02eb337eeec0a3afeaaa1469
monoxymnx <vesarthai123@gmail.com>
2024-04-04 08:22:03 +0700

    testtttt65544

--- CheckListMedicalDevice-frontend/src/pages/HomePages.tsx
+++ CheckListMedicalDevice-frontend/src/pages/HomePages.tsx
@@ -1,37 +1,18 @@
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import { AuthContext } from "../contexts/AuthContext";
-import { useContext, useState } from "react";
 import Navbar from "../components/Navbar";
-import { createTheme, ThemeProvider } from "@mui/material";
 
-const defaultTheme = createTheme({
-  palette: {
-    background: {
-      default: "#E7EFF8",
-    },
-  },
-  typography: {
-    fontFamily: ["-apple-system", "sans-serif"].join(","),
-  },
-});
 
 type Props = {};
 
 function HomePages({ }: Props) {
-  const { removeUser } = useContext(AuthContext);
-  const [message, setMessage] = useState<string>();
-
-  const navigate: NavigateFunction = useNavigate();
-
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+   
       <Navbar>
         <>
           <h1>นี่คือโฮมเพจ</h1>
         </>
       </Navbar>
-    </ThemeProvider>
+ 
 
   );
 }
